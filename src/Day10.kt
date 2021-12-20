264654b45a0864e3420f4e7163ded80627efff4a
seckinozden <ozden.seckin@gmail.com>
2021-12-20 23:55:15 +0100

    2/2 of Day 10

--- src/Day10.kt
+++ src/Day10.kt
@@ -4,6 +4,51 @@ fun main() {
     val input = readInput("Day10_input")
 
     day10Part1(input)
+    day10Part2(input)
+}
+
+fun day10Part2(input: List<String>) {
+    val pairMap = mutableMapOf(
+        ')' to '(',
+        ']' to '[',
+        '}' to '{',
+        '>' to '<',
+    )
+    val incompleteLineScores = mutableListOf<Long>()
+    input.forEach lineLoop@{ line ->
+        val stack = Stack<Char>()
+        line.toCharArray().forEach {
+            when (it) {
+                in listOf('(', '[', '{', '<') -> {
+                    stack.push(it)
+                }
+                in listOf(']', '}', ')', '>') -> {
+                    if (stack.isEmpty() || !stack.pop().equals(pairMap[it])) {
+                        // it is corrupted line
+                        // we are interested in incomplete lines
+                        return@lineLoop
+                    }
+                }
+            }
+        }
+        if (stack.isNotEmpty()) {
+            var score = 0L
+            while (stack.isNotEmpty()) {
+                score *= 5
+                when (stack.pop()) {
+                    '(' -> { score += 1 }
+                    '[' -> { score += 2 }
+                    '{' -> { score += 3 }
+                    '<' -> { score += 4 }
+                }
+            }
+            incompleteLineScores.add(score)
+        }
+    }
+    val result = incompleteLineScores.sorted()[incompleteLineScores.size / 2]
+
+    println("--- Part 2 ---")
+    println("Result= $result")
 }
 
 fun day10Part1(input: List<String>) {
